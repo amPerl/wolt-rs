6b81b72e6290b675818c347817c05c1f1f2addf7
Romet Tagobert <sgzfxn@gmail.com>
2021-06-27 15:55:40 +0300

    add tallinn/tartu all venues tests

--- tests/venue.rs
+++ tests/venue.rs
@@ -2,9 +2,14 @@ fn test_venue_results(venue_results: wolt::VenueResults) -> anyhow::Result<()> {
     let extra_keys = venue_results._extra_fields.keys().collect::<Vec<&String>>();
     assert_eq!(extra_keys, Vec::<&String>::new());
 
-    assert_eq!(venue_results.results.len(), 1);
+    assert_eq!(venue_results.status, "OK");
+    assert!(venue_results.results.is_some());
 
-    let venue = venue_results.results.first().unwrap();
+    let results = venue_results.results.unwrap();
+
+    assert_eq!(results.len(), 1);
+
+    let venue = results.first().unwrap();
 
     dbg!(&venue.active_menu);
 
@@ -49,4 +54,43 @@ async fn tartu_kauss_kampus() -> anyhow::Result<()> {
     Ok(test_venue_results(
         wolt::get_venue("5cca9fa7e44b4dfdc403b7c2").await?,
     )?)
+}
+
+#[cfg(feature = "long_test")]
+async fn test_delivery_page(page: wolt::Page) -> anyhow::Result<()> {
+    match page {
+        wolt::Page::Delivery { sections, .. } => {
+            for section in sections {
+                if let Some(section_items) = section.items {
+                    for section_item in section_items {
+                        if let Some(section_item_venue) = section_item.venue {
+                            let wolt::PageSectionItemVenue { name, id, .. } = section_item_venue;
+                            dbg!(&name, &id);
+
+                            let venue_results = wolt::get_venue(&id).await?;
+                            test_venue_results(venue_results)?;
+                        }
+                    }
+                }
+            }
+        }
+        _ => unreachable!(),
+    }
+    Ok(())
+}
+
+#[cfg(feature = "long_test")]
+#[tokio::test]
+async fn tallinn_venues() -> anyhow::Result<()> {
+    let tallinn_delivery_page = wolt::get_delivery_page(59.43, 24.75).await?;
+    test_delivery_page(tallinn_delivery_page).await?;
+    Ok(())
+}
+
+#[cfg(feature = "long_test")]
+#[tokio::test]
+async fn tartu_venues() -> anyhow::Result<()> {
+    let tartu_delivery_page = wolt::get_delivery_page(58.378, 26.729).await?;
+    test_delivery_page(tartu_delivery_page).await?;
+    Ok(())
 }
